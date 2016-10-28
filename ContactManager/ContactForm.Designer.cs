6e0eeb54213791571dd6bd4dc53f2a88de676bb5
Haou <HayateUnlucky@gmail.com>
2016-10-28 11:32:47 +0700

    Initialize project

---
+++ ContactManager/ContactForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ContactManager
+{
+    partial class ContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_Phone = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(632, 238);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(624, 212);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Index";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(612, 200);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseUp);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(624, 212);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Add";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.6537F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.34631F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_FirstName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_LastName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Phone, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Email, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button_Save, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 29);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(514, 177);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(139, 3);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(162, 20);
+            this.textBox_FirstName.TabIndex = 4;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(139, 38);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(162, 20);
+            this.textBox_LastName.TabIndex = 5;
+            // 
+            // textBox_Phone
+            // 
+            this.textBox_Phone.Location = new System.Drawing.Point(139, 73);
+            this.textBox_Phone.Name = "textBox_Phone";
+            this.textBox_Phone.Size = new System.Drawing.Size(162, 20);
+            this.textBox_Phone.TabIndex = 6;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Location = new System.Drawing.Point(139, 112);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(162, 20);
+            this.textBox_Email.TabIndex = 7;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(3, 146);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(75, 23);
+            this.button_Save.TabIndex = 8;
+            this.button_Save.Text = "Save";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 262);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ContactForm";
+            this.Text = "Contact Manager";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_Phone;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ContactManager/ContactForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ContactManager.Models;
+using System.Text.RegularExpressions;
+
+namespace ContactManager
+{
+    public partial class ContactForm : Form
+    {
+        private ContactManagerDBEntities _entities = new ContactManagerDBEntities();
+
+        public ContactForm()
+        {
+            InitializeComponent();
+
+            InitControls();
+
+            IndexView();
+        }
+
+        private void InitControls()
+        {
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void IndexView()
+        {
+            dataGridView1.DataSource = _entities.Contacts.ToList();
+
+            tabControl1.TabPages[1].Text = "Add";
+            tabControl1.SelectedTab = tabPage1;
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab == tabPage1)
+            {
+                AddView();
+            }
+        }
+
+        private void AddView()
+        {
+            tabControl1.TabPages[1].Text = "Add";
+            button_Save.Tag = null;
+            textBox_FirstName.Text = "";
+            textBox_LastName.Text = "";
+            textBox_Phone.Text = "";
+            textBox_Email.Text = "";
+        }
+
+        private void EditView(Contact contactToEdit)
+        {
+            tabControl1.TabPages[1].Text = "Edit";
+            button_Save.Tag = contactToEdit.Id;
+            textBox_FirstName.Text = contactToEdit.FirstName;
+            textBox_LastName.Text = contactToEdit.LastName;
+            textBox_Phone.Text = contactToEdit.Phone;
+            textBox_Email.Text = contactToEdit.Email;
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void dataGridView1_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //Tao menu ngu canh
+                ContextMenuStrip menu = new ContextMenuStrip();
+                menu.Items.Add("Edit", null, new EventHandler(Edit_Click));
+                menu.Items.Add("Delete", null, new EventHandler(Delete_Click));
+                //Di chuyen den dong hien hanh
+                dataGridView1.CurrentCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                //hien thi menu
+                Point pt = dataGridView1.PointToClient(Control.MousePosition);
+                menu.Show(dataGridView1, pt.X, pt.Y);
+            }
+        }
+
+        private void Edit_Click(object sender, EventArgs args)
+        {
+            int id = (int)dataGridView1.SelectedCells[0].Value;
+            var contactToEdit = (from c in _entities.Contacts
+                                 where c.Id == id
+                                 select c).FirstOrDefault();
+            EditView(contactToEdit);
+        }
+
+        private void Delete_Click(object sender, EventArgs args)
+        {
+            int id = (int)dataGridView1.SelectedCells[0].Value;
+            var contactToDelete = (from c in _entities.Contacts
+                                   where c.Id == id
+                                   select c).FirstOrDefault();
+            if (MessageBox.Show(string.Format("Do you want to delete {0} {1} ?", contactToDelete.FirstName,
+                 contactToDelete.LastName), "Delete contact", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            {
+                _entities.DeleteObject(contactToDelete);
+                _entities.SaveChanges();
+                IndexView();
+            }
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            Contact contactToEdit = GetContactToCreate();
+            if (ValidateContact(contactToEdit))
+            {
+                if (contactToEdit.Id > 0) //Contact da ton tai
+
+                {
+                    var originalContact = (from c in _entities.Contacts
+                                           where c.Id == contactToEdit.Id
+                                           select c).FirstOrDefault();
+                    _entities.ApplyCurrentValues(originalContact.EntityKey.EntitySetName, contactToEdit);
+                }
+                else //Contact moi
+                {
+                    contactToEdit.Id = _entities.Contacts .Count ()> 0 ? _entities.Contacts.Max(c => c.Id) +1 : 1;
+                    _entities.AddToContacts(contactToEdit);
+                }
+                _entities.SaveChanges();
+                IndexView();
+            }
+        }
+
+        private bool ValidateContact(Contact contactToValidate)
+        {
+            bool isValid = true;
+            errorProvider1.Clear();
+
+            if (contactToValidate.FirstName.Trim().Length == 0)
+            {
+                errorProvider1.SetError(textBox_FirstName, "First name is required.");
+                isValid = false;
+            }
+            if (contactToValidate.LastName.Trim().Length == 0)
+            {
+                errorProvider1.SetError(textBox_LastName, "Last name is required.");
+                isValid = false;
+            }
+            if (contactToValidate.Phone.Length > 0 && !Regex.IsMatch(contactToValidate.Phone, @"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}"))
+            {
+                errorProvider1.SetError(textBox_Phone, "Invalid phone number.");
+                isValid = false;
+            }
+            if (contactToValidate.Email.Length > 0 && !Regex.IsMatch(contactToValidate.Email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+            {
+                errorProvider1.SetError(textBox_Email, "Invalid email address.");
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        private Contact GetContactToCreate()
+        {
+            return new Contact()
+            {
+                Id = button_Save.Tag == null ? 0 : (int)button_Save.Tag,
+                FirstName = textBox_FirstName.Text.Trim(),
+                LastName = textBox_LastName.Text.Trim(),
+                Phone = textBox_Phone.Text.Trim(),
+                Email = textBox_Email.Text.Trim()
+            };
+        }
+
+    }
+}
